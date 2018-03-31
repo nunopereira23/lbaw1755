2492064cfaa39cbba8dba9d67296da4caa2c5fe7
TADias <tasdsilva@gmail.com>
2018-03-31 03:47:09 +0100

    added triggers and population

---
+++ IamIn/database/lbaw1755_data.sql
@@ -0,0 +1,152 @@
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (1,'mollis nec, cursus a, enim.','Nullam','2018-07-01 00:14:27','2019-01-30 09:54:25','Public','Party','-47.25371, -67.11959','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES(2,'ac','ornare.','2018-07-07 00:21:19','2019-03-07 06:15:18','Private','Party','-34.36488, 147.10962','true');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES(3,'amet lorem semper auctor.','Vivamus','2018-07-07 20:20:30','2018-12-13 05:16:51','Private','Birthday','23.39034, 146.96518','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES(4,'Suspendisse aliquet molestie tellus. Aenean','dictum','2018-07-27 01:38:28','2018-08-27 07:02:25','Private','Sport','63.91214, -78.61672','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES(5,'sed libero. Proin sed','euismod','2018-08-01 15:20:03','2018-11-03 00:46:56','Public','Trip','-81.44425, -167.51761','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES(6,'enim consequat purus. Maecenas libero','amet,','2018-07-30 10:21:41','2018-09-10 20:27:31','Private','Birthday','-18.43156, 152.18081','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES(7,'fringilla euismod enim.','nec','2018-07-04 18:32:43','2019-02-02 11:52:00','Private','Birthday','-76.3689, 131.85253','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (8,'ipsum porta','luctus','2018-07-19 02:08:08','2018-09-22 16:39:49','Public','Education','89.75846, 132.25732','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (9,'magnis dis','eget','2018-07-26 05:01:43','2019-03-21 19:20:23','Public','Birthday','-12.43291, -142.02276','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (10,'et netus et malesuada fames','Nulla','2018-07-14 18:50:18','2019-01-08 04:31:32','Private','Culture','-69.00135, -151.24385','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (11,'sociis natoque penatibus et magnis','Nulla','2018-07-20 23:55:39','2018-09-21 05:12:45','Public','Education','-19.44984, 100.98539','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (12,'dui augue eu','libero','2018-07-03 03:51:12','2018-09-21 23:09:57','Public','Sport','87.76708, 13.63634','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (13,'ipsum ac mi eleifend','Curabitur','2018-07-22 11:54:57','2018-11-15 15:49:25','Private','Culture','35.82419, -158.988','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (14,'sed sem egestas','libero.','2018-07-23 10:17:55','2019-01-10 21:00:08','Public','Party','71.99589, 6.62822','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (15,'Donec feugiat metus','odio.','2018-07-09 21:46:30','2019-01-17 08:30:44','Public','Sport','-17.54719, 130.42452','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (16,'urna, nec luctus felis purus','montes,','2018-07-02 22:56:24','2018-08-16 20:24:23','Public','Birthday','-7.61421, -165.48763','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (17,'Vestibulum ante ipsum primis in','lorem,','2018-07-17 00:02:49','2018-12-07 05:08:48','Public','Party','22.95228, 110.99558','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (18,'Phasellus vitae mauris sit','nec','2018-07-28 19:59:51','2019-01-27 10:41:37','Private','Party','-70.37884, -17.9159','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (19,'commodo at,','imperdiet','2018-07-02 22:19:16','2019-03-11 08:08:32','Public','Birthday','74.89323, 90.64342','false');
+INSERT INTO "event" (id,description,title,event_start,event_end,event_visibility,event_type,gps,is_deleted) VALUES (20,'Suspendisse eleifend. Cras sed','pharetra','2018-07-15 14:10:08','2018-12-24 01:53:03','Private','Culture','46.44343, 47.03385','false');
+
+
+
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (1,'Suspendisse@gravidanuncsed.ca','Emily','Oct 4, 1946',1,'2546626027DFA7B2DB0B44DF9886779EDFC31C063773DD412D25FC21BFEF64F0','profile/1','false','true');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (2,'sem.vitae@ametconsectetuer.net','Wyatt','Dec 24, 1949',0,'5FD924625F6AB16A19CC9807C7C506AE1813490E4BA675F843D5A10E0BAACDB8','profile/2','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (3,'non.dapibus@ornarelectusante.org','Nyssa','Apr 13, 1946',3,'5FD924625F6AB16A19CC9807C7C506AE1813490E4BA675F843D5A10E0BAACDB8','profile/3','true','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (4,'feugiat@semperpretium.edu','Clinton','May 24, 1960',2,'1DE86DC4F11D8BD2E5C49DA284DA83D27B9CDE4E0A3F304529E89AEF5088BA69','profile/4','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (5,'vel.arcu.eu@vitae.ca','George','Oct 29, 1963',0,'50AD41624C25E493AA1DC7F4AB32BDC5A3B0B78ECC35B539936E3FEA7C565AF7','profile/5','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (6,'nulla@Curabituregestas.net','Germane','Apr 12, 1982',2,'BC52D6BFE3AC965E069109DBD7D15E0CCAAA55678F6E2A6664BEE2EDF8AE1B2B','profile/6','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (7,'Ut.semper@pharetranibh.org','Samson','Dec 21, 1952',2,'18EE24150DCB1D96752A4D6DD0F20DFD8BA8C38527E40AA8509B7ADECF78F9C6','profile/7','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (8,'sem.consequat@tempor.net','Allistair','Jan 24, 1962',1,'824A61827555517927F87E16A9EC638BA1369F87270EBBA3275370DC24307559','profile/8','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (9,'quis.massa.Mauris@tempuseuligula.net','Jillian','Mar 22, 1962',2,'645A387A1D5F5BEC4CB243E81091049CD9A07F7A033D86A3615028442B3B4E52','profile/9','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (10,'lorem.eget@adipiscing.edu','Alden','Jun 30, 2000',0,'E797C0013811A1D1E35AD7EDD10FB99986DB664B0996C76ED9AE5E0A5151BBF9','profile/10','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (11,'nisi.sem.semper@orci.net','Ryan','Apr 29, 1949',0,'048C79AD93713E3759DCCFE3C05059FF8CCBF0445FB21D08DE9F255541C8F01B','profile/11','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (12,'quam@Duisac.edu','Castor','Sep 29, 1963',2,'96CAE35CE8A9B0244178BF28E4966C2CE1B8385723A96A6B838858CDD6CA0A1E','profile/12','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (13,'dictum.eu@congue.com','Chase','Oct 21, 1973',2,'60821E944E285931636B2C4215F2AA70F197E515E4B366C191FCCCDC89679AD4','profile/13','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (14,'scelerisque@esttempor.ca','Flavia','Jul 5, 2001',2,'18AC3E7343F016890C510E93F935261169D9E3F565436429830FAF0934F4F8E4','profile/14','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (15,'sed.consequat.auctor@velitduisemper.org','Ulysses','Jul 6, 1940',0,'50E721E49C013F00C62CF59F2163542A9D8DF02464EFEB615D31051B0FDDC326','profile/15','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (16,'sodales.nisi.magna@ipsum.edu','Rama','Aug 15, 1984',0,'DE04D58DC5CCC4B9671C3627FB8D626FE4A15810BC1FE3E724FEEA761965FB71','profile/16','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (17,'ligula.elit.pretium@semut.edu','Wyatt','Jun 30, 1946',2,'AE0305A9427A91F6F63E55AF0EAA1D9C4C02AF07F672D15E4A77D99B65327822','profile/17','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (18,'Nullam@mattisvelitjusto.net','Armando','Aug 4, 1945',2,'867BE73160E29FF5BE8ECACD417AE02B8B5E78E7A06C4216CA40F4D4026C99C8','profile/18','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (19,'lacus.Aliquam.rutrum@Sed.net','Mannix','Jan 14, 1994',1,'066CC3696D4E2CEDFA12902D2A5714A0AB7DDE2353DB1B56AEE861F4BDABDD1C','profile/19','false','false');
+INSERT INTO "user" (id,email,name,birthdate,nr_warnings,password_hash,profile_picture_path,is_banned,is_admin) VALUES (20,'Nam.nulla.magna@atfringillapurus.com','Stephanie','Aug 26, 1956',2,'60821E944E285931636B2C4215F2AA70F197E515E4B366C191FCCCDC89679AD4','profile/20','false','false');
+
+
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (8,10,'Owner');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (20,6,'Owner');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (2,13,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (13,6,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (16,1,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (3,20,'Owner');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (15,6,'Owner');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (10,1,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (13,12,'Invited');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (17,4,'Invited');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (2,15,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (12,9,'Owner');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (8,3,'Going');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (7,14,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (9,11,'Owner');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (16,3,'Going');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (5,10,'Ignoring');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (4,18,'Invited');
+INSERT INTO "event_user" (id_event,id_user,event_user_state) VALUES (10,11,'Invited');
+
+
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (1,17,7,'justo eu arcu. Morbi','2018-12-01 06:11:36');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (2,10,6,'nunc. Quisque ornare tortor','2017-10-01 01:38:24');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (3,7,7,'odio tristique pharetra. Quisque ac','2018-01-06 16:53:55');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (4,7,7,'Integer vitae nibh.','2018-12-08 07:16:13');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (5,12,11,'gravida. Aliquam','2017-11-02 09:29:00');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (6,10,9,'facilisis, magna tellus faucibus leo,','2017-12-13 07:24:36');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (7,11,7,'Morbi vehicula. Pellentesque tincidunt','2019-02-12 21:59:59');
+INSERT INTO "comment" (id,id_event,id_user,comment_content,date) VALUES (8,19,4,'libero est, congue a, aliquet','2018-11-18 17:55:59');
+
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (1,3,10,'aliquet magna a neque. Nullam ut nisi a odio semper','mauris. Suspendisse aliquet molestie');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (2,14,9,'arcu. Vestibulum ut eros non enim commodo hendrerit.','nec urna et');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (3,5,13,'aliquet magna a neque. Nullam ut nisi a','at, velit. Pellentesque ultricies');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (4,1,11,'enim mi tempor lorem, eget mollis lectus pede et risus.','Integer');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (5,15,18,'luctus.','Suspendisse tristique neque venenatis');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (6,6,9,'risus. Duis a mi fringilla mi lacinia mattis. Integer','id');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (7,10,17,'cursus non, egestas a, dui. Cras pellentesque. Sed dictum. Proin','tempus');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (8,6,19,'et netus et malesuada fames ac turpis','Morbi');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (9,6,9,'lobortis. Class aptent taciti sociosqu ad litora torquent per conubia','tortor at risus.');
+INSERT INTO "poll" (id,id_event,id_user,description,question) VALUES (10,1,19,'posuere vulputate, lacus. Cras interdum. Nunc','sodales');
+
+INSERT INTO "answer" (id,id_poll,answer) VALUES (1,6,'diam. Pellentesque habitant');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (2,4,'arcu');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (3,4,'interdum ligula');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (4,7,'lacinia. Sed');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (5,3,'sollicitudin');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (6,2,'sem magna nec');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (7,8,'tempus scelerisque,');
+INSERT INTO "answer" (id,id_poll,answer) VALUES (8,1,'Nunc sed orci');
+
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (2,6);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (1,9);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (1,11);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (3,19);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (6,19);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (4,5);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (1,10);
+INSERT INTO "answer_user" (id_answer,id_user) VALUES (2,15);
+
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (1,'media/1','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (2,'media/2','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (3,'media/3','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (4,'media/4','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (5,'media/5','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (6,'media/6','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (7,'media/7','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (8,'media/8','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (9,'media/9','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (11,'media/11','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (10,'media/10','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (12,'media/12','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (13,'media/13','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (14,'media/14','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (15,'media/15','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (16,'media/16','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (17,'media/17','Picture');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (18,'media/18','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (19,'media/19','Video');
+INSERT INTO "path" (id,path_value,multimedia_type) VALUES (20,'media/20','Video');
+
+
+INSERT INTO "event_path" (id_path,id_event) VALUES (1,1);
+INSERT INTO "event_path" (id_path,id_event) VALUES (2,2);
+INSERT INTO "event_path" (id_path,id_event) VALUES (3,3);
+INSERT INTO "event_path" (id_path,id_event) VALUES (4,4);
+INSERT INTO "event_path" (id_path,id_event) VALUES (5,5);
+INSERT INTO "event_path" (id_path,id_event) VALUES (6,6);
+INSERT INTO "event_path" (id_path,id_event) VALUES (7,7);
+INSERT INTO "event_path" (id_path,id_event) VALUES (8,8);
+INSERT INTO "event_path" (id_path,id_event) VALUES (9,9);
+INSERT INTO "event_path" (id_path,id_event) VALUES (10,10);
+INSERT INTO "event_path" (id_path,id_event) VALUES (11,11);
+INSERT INTO "event_path" (id_path,id_event) VALUES (12,12);
+INSERT INTO "event_path" (id_path,id_event) VALUES (14,14);
+INSERT INTO "event_path" (id_path,id_event) VALUES (15,15);
+INSERT INTO "event_path" (id_path,id_event) VALUES (16,16);
+INSERT INTO "event_path" (id_path,id_event) VALUES (17,17);
+INSERT INTO "event_path" (id_path,id_event) VALUES (18,18);
+INSERT INTO "event_path" (id_path,id_event) VALUES (19,19);
+INSERT INTO "event_path" (id_path,id_event) VALUES (20,20);
+
+
+INSERT INTO "report" (id,id_user,description) VALUES (1,13,'varius. Nam porttitor scelerisque');
+INSERT INTO "report" (id,id_user,description) VALUES (2,4,'nec, mollis vitae, posuere');
+INSERT INTO "report" (id,id_user,description) VALUES (3,14,'mattis ornare, lectus ante');
+INSERT INTO "report" (id,id_user,description) VALUES (4,19,'neque. Sed');
+INSERT INTO "report" (id,id_user,description) VALUES (5,6,'dictum. Phasellus in felis.');
