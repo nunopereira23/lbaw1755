99756c1219dd1ec637efe3a8e760e4f10e5dd412
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-04-10 18:52:05 +0100

    added credentials to upload image

--- upload_image.sh
+++ upload_image.sh
@@ -3,8 +3,8 @@
 # Stop execution if a step fails
 set -e
 
-DOCKER_USERNAME=tiagoboldt # Replace by your docker hub username
-IMAGE_NAME=lbaw-demo
+DOCKER_USERNAME=lbaw1755
+IMAGE_NAME=lbaw1755
 
 # Ensure that dependencies are available
 composer install
