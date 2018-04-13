efa267a9fb3d5054e40a6b6876532e3d584c2fa8
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-04-13 13:56:01 +0100

    GET about page

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
+
+// About
+Route::get('about', 'AboutController@show');
