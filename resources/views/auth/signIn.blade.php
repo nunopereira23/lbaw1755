c4421a3ac1e73e86654d6541c6d750dc3c49b328
Lenka Janosova <janosova.lenka@gmail.com>
2018-04-13 14:46:44 +0100

    sign in and sign up

--- resources/views/auth/signIn.blade.php
+++ resources/views/auth/signIn.blade.php
@@ -63,13 +63,12 @@
 
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 
-                <form id="loginform" class="form-horizontal" role="form" method="POST" action='verify'>
-                    {{ csrf_field() }}
+                <form id="loginform" class="form-horizontal" role="form">
 
                     <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                         <input id="login-username" type="text" class="form-control" name="username" value=""
-                               placeholder="email">
+                               placeholder="username or email">
                     </div>
 
                     <div style="margin-bottom: 25px" class="input-group">
@@ -92,7 +91,7 @@
                         <!-- Button -->
 
                         <div class="col-sm-12 controls">
-                            <input id="btn-login" type="submit"  class="btn btn-success"> </input>
+                            <a id="btn-login" href="profile.html" class="btn btn-success">Login </a>
                             <a class="btn  btn-social-icon btn-google">
                                 <span class="fa fa-google"></span> Sign in with Google
                             </a>
