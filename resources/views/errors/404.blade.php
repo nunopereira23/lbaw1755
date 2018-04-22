77e36634d0cbd196f6bd11bebb63d7903e555fe2
Nuno <up201506265@fe.up.pt>
2018-04-23 00:47:57 +0100

    some bug fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -54,7 +54,7 @@
         <div class="text-center"><i class="fa fa-5x fa-frown-o" style="color:#d9534f;"></i></div>
         <h1 class="text-center">404 Not Found<p> </p><p><small class="text-center"> Something wrong happen</small></p></h1>
         <p class="text-center">Try pressing the back button or clicking on this button.</p>
-<<<<<<< HEAD
+
         <p class="text-center"><a class="btn btn-primary" href="#"><i class="fa fa-home"></i> Take Me Home</a></p>
     </div>
 </div>
@@ -90,10 +90,10 @@
 
 </body>
 </html>
-=======
+
         <p class="text-center"><a class="btn btn-primary" href="{{ route('home') }}"><i class="fa fa-home"></i> Take Me Home</a></p>
     </div>
 </div>
 </body>
 </html>
->>>>>>> f3d3588c8022b64ab70450c58912ebba30bb9dba
+
