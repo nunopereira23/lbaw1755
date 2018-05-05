dee4ee8e1f311e6177a498ff8d24640f4e519d35
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-05-05 22:36:50 +0100

    fixed faq .blade file

--- resources/views/partials/faq.blade.php
+++ resources/views/partials/faq.blade.php
@@ -2,13 +2,13 @@
 
 <div class="container">
     <br>
-    <h4>Frequently asked questions</h4>
+    <h1>Frequently asked questions</h1>
     <br>
     <div class="container" id="accordion">
         <div class="card">
             <div class="card-header">
                 <a class="card-link" data-toggle="collapse" href="#collapseOne">
-                    How can I use "I am in!?
+                    <p>How can I use "I am in!?</p>
                 </a>
             </div>
             <div id="collapseOne" class="collapse" data-parent="#accordion">
@@ -16,13 +16,15 @@
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
                     wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
                     excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
                 </div>
             </div>
         </div>
+        <br/>
         <div class="card">
             <div class="card-header">
                 <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
-                    How can I search for events?
+                    <p>How can I search for events?</p>
                 </a>
             </div>
             <div id="collapseTwo" class="collapse" data-parent="#accordion">
@@ -33,10 +35,11 @@
                 </div>
             </div>
         </div>
+        <br/>
         <div class="card">
             <div class="card-header">
                 <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
-                    How can I create event?
+                    <p>How can I create event?</p>
                 </a>
             </div>
             <div id="collapseThree" class="collapse" data-parent="#accordion">
@@ -47,10 +50,11 @@
                 </div>
             </div>
         </div>
+        <br/>
         <div class="card">
             <div class="card-header">
                 <a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">
-                    How can I invite friends for my event?
+                    <p>How can I invite friends for my event?</p>
                 </a>
             </div>
             <div id="collapseFour" class="collapse" data-parent="#accordion">
