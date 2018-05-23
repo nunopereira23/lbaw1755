564e39205c40908e22deac48d3a8993ceda6c9b9
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-05-23 14:42:03 +0100

    changes in homepage

--- resources/views/partials/homepage.blade.php
+++ resources/views/partials/homepage.blade.php
@@ -1,3 +1,6 @@
 <link href="{{ asset('css/homepage.css') }}" rel="stylesheet">
 
-<img src="{{ asset('../images/concert.jpeg') }}" style="width:100%" class="img-responsive">
+<div class="embed-responsive">
+    <img src="{{ asset('../images/concert.jpeg') }}" style="width:100%" class="img-fluid">
+    <div class="centered"><p><b>Welcome to <i>I AM IN!</i></b></p></div>
+</div>
