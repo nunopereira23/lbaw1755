62ed24e30c59836bc3a4a4929cd1d03a997973ae
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-05-23 03:18:08 +0100

    added image upload to registration page

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'birthdate',
+        'name', 'email', 'nr_warnings', 'password', 'birthdate', 'profile_picture_path',
     ];
 
     /**
