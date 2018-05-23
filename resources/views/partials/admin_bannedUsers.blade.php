d090491a32200d6b72a8847a0583c0d6b14be85c
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-05-23 15:04:21 +0100

    improvements in admin pages

--- resources/views/partials/admin_bannedUsers.blade.php
+++ resources/views/partials/admin_bannedUsers.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="administration.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<body>
+<link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 <div class="container">
     <h2>Banned Users</h2>
     <br>
@@ -27,7 +18,7 @@
             <td>
                 <form method="post" action="/users/<?php echo $user->id ?>/reinstate">
                     {{ csrf_field() }}
-                    <input type="submit" name="action" value="Reinstate"/>
+                    <input type="submit" name="action" class="btn btn-success" value="Reinstate"/>
                     <input type="hidden" name="id" value="<?php echo $user->id; ?>"/>
                 </form>
             </td>
